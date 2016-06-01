b96525463d951206ee53a5f36cb9ed0b6a9b49a0
Richard Joe <rjoe19@hotmail.com>
2016-06-01 18:53:50 +1200

    setup server

--- server.js
+++ server.js
@@ -11,3 +11,30 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080 //set our PORT
+
+
+
+//ROUTES FOR OUR API
+//--------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------
+
+var router = express.Router();      // get an instance of the express router
+
+// test route to make sure everything is working )accessed at GET http://localhost:8080/
+router.get('/', function(req, res){
+  res.json({message:'hooray! welcome to our API'});
+});
+
+// more routes for our API will happen here
+
+
+// REGISTER OUR ROUTES ------------------------------------
+//All of our routes will be prefixed with /API
+app.use('/api', router);
+
+
+
+// START THE SERVER
+//--------------------------------------------------------------------------------------------
+app.listen(port);
+console.log('Magic happens on port' + port);
